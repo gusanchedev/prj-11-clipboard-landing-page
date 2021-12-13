0044711bf5f366c9c9c1bec8a053ee9fd7f4daa7
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-12-13 11:35:40 -0500

    💄  Styling classes for mobile 375px

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bai: ["Bai Jamjuree", "sans-serif"],
+      },
+      fontSize: {
+        "project": "1.125rem"
+      },
+      colors: {
+       "strongCyan": "hsl(171, 66%, 44%)",
+       "lightBlue": "hsl(233, 100%, 69%)",
+       "darkGrayishBlue": "hsl(210, 10%, 33%)",
+       "grayishBlue": "hsl(201, 11%, 66%)",
+      }
+    },
   },
   plugins: [],
 }
