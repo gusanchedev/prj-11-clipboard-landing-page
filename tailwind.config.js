48a891e6b26b07d932bf9074cd80236fe634cab6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-12-31 16:07:46 -0500

    Use tailwind @apply for similar styling in html elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         bai: ["Bai Jamjuree", "sans-serif"],
       },
       fontSize: {
-        "project": "1.125rem"
+        project: "1.125rem",
       },
       colors: {
-       "strongCyan": "hsl(171, 66%, 44%)",
-       "lightBlue": "hsl(233, 100%, 69%)",
-       "darkGrayishBlue": "hsl(210, 10%, 33%)",
-       "grayishBlue": "hsl(201, 11%, 66%)",
-      }
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/images/bg-header-mobile.png')",
+      },
     },
   },
   plugins: [],
-}
+};
